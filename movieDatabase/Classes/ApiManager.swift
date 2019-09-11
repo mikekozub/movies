895d75d7db4d23c81691785229c88dd9e62d9b32
Michael Kozub <michaelkozub@gmail.com>
2019-09-11 07:13:25 -0600

    Basic networking functions

--- movieDatabase/Classes/ApiManager.swift
+++ movieDatabase/Classes/ApiManager.swift
@@ -10,4 +10,30 @@ import UIKit
 
 class APIManager {
     
+    func loadJsonFromUrl() -> Data? {
+        do {
+            let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a020691c4cf21a30210d62ca7b841e5a")!
+            let data = try Data(contentsOf: url)
+            print(data)
+            return data
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+    
+    func deserializeData(loadedData: Data, onCompletion: @escaping (_ toilets: [MovieDataModel]) -> Void){
+        DispatchQueue.global().async {
+            do {
+                let decoder = JSONDecoder()
+                let moviesDecoded = try decoder.decode(ApiResponse.self, from: loadedData)
+                DispatchQueue.main.async {
+                    onCompletion(moviesDecoded.results)
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
 }
