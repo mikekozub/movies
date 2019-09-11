70b9e2d41d75036aab4513fff4c10777fe92d6d5
Michael Kozub <michaelkozub@gmail.com>
2019-09-11 07:12:39 -0600

    Created simple data model

--- movieDatabase/Model/MovieDataModel.swift
+++ movieDatabase/Model/MovieDataModel.swift
@@ -9,9 +9,19 @@
 import Foundation
 
 struct ApiResponse: Codable {
-
+    let success: Bool?
+    let message: String?
+    let results: [MovieDataModel]
 }
 
 class MovieDataModel: NSObject, Codable {
-
+    var id: Int?
+    var title: String?
+    var imageUrl: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case imageUrl = "poster_path"
+    }
 }
